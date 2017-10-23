24a45803dda80ae087cacb5ee458bf41510f49a4
Csipke Ferenc <csipke.ferenc@gmail.com>
2017-10-23 18:48:13 +0200

    Update menu generator

--- src/Shell/MenuGeneratorShell.php
+++ src/Shell/MenuGeneratorShell.php
@@ -52,7 +52,7 @@ class MenuGeneratorShell extends Shell {
 		$menuHtml = '<ul class="sidebar-menu" data-widget="tree">
 	<li class="header"><?php echo __(\'MAIN NAVIGATION\');?></li>
 	<li>
-    	<a href="<?php echo $this->Url->build([\'controller\' => \'Pages\', \'action\' => \'dashboard\']); ?>">
+    	<a href="<?php echo $this->Url->build([\'controller\' => \'Pages\', \'action\' => \'home\']); ?>">
         	<i class="fa fa-dashboard"></i> <span>Dashboard</span>
 		</a>
 	</li>';
